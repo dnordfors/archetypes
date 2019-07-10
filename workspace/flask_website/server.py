38234d63a7b253b8ccf444461f6197115a92bba5
dnordfors <dnordfors@gmail.com>
2019-07-10 14:04:23 -0700

    working homepage

--- workspace/flask_website/server.py
+++ workspace/flask_website/server.py
@@ -2,10 +2,11 @@
 from flask import Flask,render_template, request,jsonify,Response
 #Create the app object that will route our calls
 app = Flask(__name__)
-# Add a single endpoint that we can use for testing
+# Route the user to the homepage
 @app.route('/', methods = ['GET'])
 def home():
-    return '<h1> Hello World </h1><p>My name is Elliott</p>'
+    return render_template('home.html')
+
 #When run from command line, start the server
 if __name__ == '__main__':
     app.run(host ='0.0.0.0', port = 3333, debug = True)
